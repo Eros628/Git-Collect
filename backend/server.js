d1006196db6113a40c6d2b94984bca72800be685
Eros628 <eroslucagbo314@gmail.com>
2026-05-16 13:23:44 +0800

    feat: Added new api end point for gettign all repos of the user

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,28 @@
 import express from 'express';
-import { searchByRepos, searchByUser } from './services/api.js';
+import { getUserAllRepos, searchByRepos, searchByUser } from './services/api.js';
 
 const app = express();
 
 
-app.get("/gitcollect/api/repositories", async (req, res)=>{
+app.get("/gitcollect/api/search/repositories", async (req, res)=>{
     console.log(req);
     const {q, sort, page, order } = req.query;
     const response =  await searchByRepos({q, sort, page, order});
     res.send(response);
 });
 
-app.get("/gitcollect/api/:user", async(req, res)=>{
+app.get("/gitcollect/api/search/:user", async(req, res)=>{
     const {q, sort, page, order} = req.query;
     const user = req.params.user;
     const data = await searchByUser({user, q, sort, page, order});
     res.send(data.data);
-})  
+});
+
+app.get("/gitcollect/api/:user/repos", async(req, res)=>{
+    const user = req.params.user;
+    const data = await getUserAllRepos(user);
+    res.send(data);
+});
 
 app.listen(3000, ()=>{
     console.log(`THE SERVER IS RUNNING ON: `)
--- backend/services/api.js
+++ backend/services/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { formToJSON } from "axios";
 import { config } from "dotenv";
 import 'dotenv/config.js';
 
@@ -104,9 +104,36 @@ export async function searchByUser(params) {
 
 }
 
-export async function getUserRepos(params){
+export async function getUserAllRepos(user){
     try {
-        const response = await searchApi
+        const response = await gitAuth.get(`/users/${user}/repos`);
+        const response_starred_repo = await gitAuth.get(`/users/${user}/starred`);
+
+        const repos = await Promise.all( response.data.map(async (repo, index) =>{
+            let isStarred = response_starred_repo.data.some(starItem => starItem.id === repo.id) ? "true" : "false";
+            const languages = await gitAuth.get(repo.languages_url);
+           
+
+            const item = {
+                id: repo.id,
+                name: repo.name,
+                full_name: repo.full_name,
+                html_url: repo.html_url,
+                description: repo.description,
+                created_at: repo.created_at,
+                updated_at: repo.updated_at,
+                git_url: repo.git_url,
+                ssh_url: repo.ssh_url,
+                stargazers_count: repo.stargazers_count,
+                languages: languages.data,
+                isStarred: isStarred
+            };
+
+            return item;
+        }));
+
+        return repos;
+       
     } catch (error) {
         console.log(error);
     }
